5efb8dcfd574747ce41d34422e8eeb6bdcc5585b
colind0pe <71618029+colind0pe@users.noreply.github.com>
2021-12-18 01:08:05 +0800

    Update awvs.sh

--- awvs.sh
+++ awvs.sh
@@ -5,4 +5,4 @@ docker run -d -it -p 3443:3443 --restart=always --add-host=updates.acunetix.com:
 	--add-host=erp.acunetix.com:127.0.0.1 --name awvs xrsec/awvs
 docker cp license_info.json awvs:/home/acunetix/.acunetix/data/license/
 docker cp wa_data.dat  awvs:/home/acunetix/.acunetix/data/license/
-docker exec awvs chmod -w /home/acunetix/.acunetix/data/license/license_info.json
+docker exec awvs chmod 444 /home/acunetix/.acunetix/data/license/license_info.json
